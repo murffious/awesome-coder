d0ab528a5c67026feec585efb04154576467fbe4
Paul Murff <titan.murff@gmail.com>
2019-03-09 11:31:28 -0700

    Create theTool.js

---
+++ theTool.js
@@ -0,0 +1,11 @@
+## Coding Process Tool . dev get it hosted 
+
+- 1. process of problems solving 
+2. learning 
+3. debugging 
+
+study tracker 
+
+jobs board 
+interviews tracker - anouncements - questions  had 
+Best bractices 
